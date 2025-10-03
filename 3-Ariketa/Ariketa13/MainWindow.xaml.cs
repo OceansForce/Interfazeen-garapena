0ab2e368c122731c7d16afd74295fc741d862b8f
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-10-03 11:05:31 +0200

    3. Ariketa bukatuta

--- 3-Ariketa/Ariketa13/MainWindow.xaml.cs
+++ 3-Ariketa/Ariketa13/MainWindow.xaml.cs
@@ -16,9 +16,52 @@ namespace Ariketa13
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public string texto = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Salir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Cortar(object sender, RoutedEventArgs e)
+        {
+            texto = textBox.Text;
+            textBox.Text = "";
+        }
+        private void Copiar(object sender, RoutedEventArgs e)
+        {
+            texto = textBox.Text;
+        }
+        private void Pegar(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = texto;
+        }
+        private void Eliminar(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+        }
+
+        private void Arial(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Arial");
+        }
+        private void Courier(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Courier New");
+        }
+        private void Impact(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Impact");
+        }
+        private void Symbol(object sender, RoutedEventArgs e)
+        {
+            textBox.FontFamily = new FontFamily("Symbol");
         }
     }
 }
