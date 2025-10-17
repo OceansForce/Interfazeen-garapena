5e75829eb4f63e8c516a0588c89b9b45843d468e
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-10-17 14:01:50 +0200

    3. gailarekin asten

---
+++ 3. UNITATEA Ikusizko osagaiak sortzea/3UD_Ariketa - Ikus-Osagaiak sortzea/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _3UD_Ariketa___Ikus_Osagaiak_sortzea
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
